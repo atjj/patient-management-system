eb298a2be7f8fbbb3f8cf5cdbfb7e3afe141f0ab
Atay <atajmamasadykov@gmail.com>
2026-05-06 13:31:51 +0600

    remove comment text from src>lib>validation.ts

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -91,24 +91,3 @@ export const PatientFormValidation = z.object({
       message: "You must consent to privacy in order to proceed",
     }),
 });
-
-/*   name: string;
-  email: string;
-  phone: string;
-  birthDate: Date;
-  gender: Gender;
-  address: string;
-  occupation: string;
-  emergencyContactName: string;
-  emergencyContactNumber: string;
-  primaryPhysician: string;
-  insuranceProvider: string;
-  insurancePolicyNumber: string;
-  allergies: string | undefined;
-  currentMedication: string | undefined;
-  familyMedicalHistory: string | undefined;
-  pastMedicalHistory: string | undefined;
-  identificationType: string | undefined;
-  identificationNumber: string | undefined;
-  identificationDocument: FormData | undefined;
-  privacyConsent: boolean; */
